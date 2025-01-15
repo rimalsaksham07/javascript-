b0b94a50d162fb51e67690218d3a7c689fab3450
Saksham_rimal <138751281+rimalsaksham07@users.noreply.github.com>
2025-01-15 15:59:08 +0000

    new commit

---
+++ 01_basics/01_version.js
@@ -0,0 +1,12 @@
+const accountId = 12344
+let accountEmail = "smth@gmail.com"
+var accountPassword = "12345"
+accountCity = "Ktm"
+
+// accountId = 2
+console.log(accountId)
+accountEmail = "new@gmail.com"
+accountPassword = "84793"
+accountCity = "BKT"
+
+console.table([accountEmail  , accountPassword , accountCity])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,19 @@
+"use strict" //treat all js code as newer version
+
+//alert(3+3)
+let name = "Saksham"
+let age =18
+let isLoggedIn = false
+
+// number => 2 to power of 53 is the range of number
+// bigint
+// string => " "
+// boolean => true/false
+// null => standalone value
+// undefined => no value given
+// symbol => unique 
+
+// object 
+
+console.log(typeof(null)) //object
+console.log(typeof(undefined)) //undefined nai hunxa 
