bc96ad5689182ef9986442b8182dca9a65745950
Saksham_rimal <138751281+rimalsaksham07@users.noreply.github.com>
2025-01-16 15:18:37 +0000

    new commit

---
+++ 01_basics/nums_and_math.js
@@ -0,0 +1,26 @@
+const score = 400
+// console.log(score);
+
+const balance = 100
+// console.log(balance);
+
+new_balance = balance.toString();
+// console.log(typeof(new_balance))
+
+const other_number = 123.893
+// console.log(other_number.toPrecision(3))
+
+const hundreds = 100000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+
+// ------maths---------------------
+
+console.log(Math);
+
+console.log(Math.round(Math.random() * 10) + 1);
+
+const min = 10
+const max = 20 
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min )
