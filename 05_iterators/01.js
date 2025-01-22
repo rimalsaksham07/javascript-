b6939bc920ba3af9cdf437432b0f67a23544cd4e
Saksham_rimal <138751281+rimalsaksham07@users.noreply.github.com>
2025-01-22 14:02:46 +0000

    new commit

--- 05_iterators/01.js
+++ 05_iterators/01.js
@@ -1,34 +1,85 @@
-// // for 
-// element = 0
-// for (let index = 0; index < 10 ; index++) {
-//     const element = index 
-//     element = element + 1   
-// }
-// console.log(element);
+// // // // for 
+// // // element = 0
+// // // for (let index = 0; index < 10 ; index++) {
+// // //     const element = index 
+// // //     element = element + 1   
+// // // }
+// // // console.log(element);
 
-// for (let i = 0; i <= 10; i++) {
-//     console.log(`outer loop value : ${i}`);
+// // // for (let i = 0; i <= 10; i++) {
+// // //     console.log(`outer loop value : ${i}`);
     
-//     for (let index = 0; index < array.length; index++) {
+// // //     for (let index = 0; index < array.length; index++) {
     
+// // //     }
+// // //     const element = array[index];
+    
+// // // }
+
+// // // keywords 
+
+// // for(let index = 1 ; index <= 20 ; index++){
+// //     if(index == 5){
+// //         continue
+// //     }
+// //     // console.log(`value of i is ${index}`);
+// // }
+
+// // for(let index = 1 ; index <= 20 ; index++){
+// //     if(index == 5){
+// //         break
+// //     }
+// //     console.log(`value of i is ${index}`);
+// // }
+
+// // const arr = [10, 20, 5, 15];
+// //console.log(findLargest(arr)); // Output: 20
+
+// const arr = [10,20,15,35,5]
+// max = 0
+// for(i = 0 ; i < arr.length-1 ; i++){
+//     if(arr[i] > arr[i + 1]){
+//         max = i
 //     }
-//     const element = array[index];
-    
 // }
+// // console.log(arr[max]);
 
-// keywords 
+// // // const arr = [1, 2, 3, 4];
+// // console.log(sumArray(arr)); // Output: 10
 
-for(let index = 1 ; index <= 20 ; index++){
-    if(index == 5){
-        continue
-    }
-    // console.log(`value of i is ${index}`);
-}
+// const arr1 = [1,2,3,4]
+// function sum(arr){
+//     let sum_arr = 0
+//     for (i=0 ; i< arr1.length  ; i++){
+//         sum_arr = sum_arr + arr[i]
+//     }
+//     // console.log(sum_arr); 
+// }
+// sum(arr1)
 
-for(let index = 1 ; index <= 20 ; index++){
-    if(index == 5){
-        break
-    }
-    console.log(`value of i is ${index}`);
-}
+//  const arr = [5, 10, 15, 20];
+// // console.log(contains(arr, 10)); // Output: true
+
+// function cont(value , arr){
+//    find = 0
+//    for(i = 0 ; i < arr.length ; i++){
+
+//     if(value == arr[i]){
+//         find = arr[i]
+//     }
+//    }
+//    console.log(find);
+// }
+// cont(10 , arr)
+// cont(25 , arr)
+
+// const arr = [1, 2, 3, 2, 1, 2];
+// console.log(countOccurrences(arr, 2)); // Output: 3
+// const arr = [1,2,3,2,1,2]
+// function repeat(){
+//     for(i =0 ; i< arr.length ; i++){
+
+//     }
+// }
+// repeat()
 
